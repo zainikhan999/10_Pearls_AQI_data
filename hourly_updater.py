a73ef4b7c9feed0f0d77d9914acfbd973b092237
zainikhan999 <zk.032918@gmail.com>
2025-08-02 01:53:16 +0500

    ready for deployment

--- hourly_updater.py
+++ hourly_updater.py
@@ -4,7 +4,8 @@ import pandas as pd
 import requests
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
-API_KEY = os.environ["API"]
+API_KEY = os.environ["HOPSWORKS_API_KEY"]
+"]
 # --- Config ---
 FG_NAME = "aqi_weather_features"
 FG_VERSION = 1
